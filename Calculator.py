60706398cb9b55a2ac0fd0bdd1652dfe82d8c068
ChosenFate62 <133608334+ChosenFate62@users.noreply.github.com>
2023-06-13 17:56:06 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+
+main = tk.Tk()
+
+userInput = tk.Variable()
+
+Number = tk.Entry(main, width = 35, borderwidth = 5,textvariable = userInput)
+Number.grid(row = 0, column = 2, columnspan  = 3, padx = 10, pady = 10)
+
+
+def button_add(value):
+    Number.insert(tk.END, value)
+    
+def operator(con):
+    global plus
+    plus = userInput.get()
+    Number.delete(0, tk.END)
+    global operation
+    operation = con
+    
+
+def equal(a):
+    second = userInput.get()
+    if a == "+":
+        mo = int(plus) + int(second)
+    elif a == "-":
+        mo = int(plus) - int(second)
+        
+    Number.delete(0,tk.END)
+    Number.insert(tk.END,mo)
+
+
+
+#define Buttons
+button_1 = tk.Button(main, text = "1", padx = 40, pady =20, command = lambda:button_add(1))
+button_1.grid(column = 1,row = 1)
+button_2 = tk.Button(main, text = "2", padx = 40, pady =20, command = lambda:button_add(2))
+button_2.grid(column = 2,row = 1)
+button_3 = tk.Button(main, text = "3", padx = 40, pady =20, command = lambda:button_add(3))
+button_3.grid(column = 3,row = 1)
+button_4 = tk.Button(main, text = "4", padx = 40, pady =20, command = lambda:button_add(4))
+button_4.grid(column = 1,row = 2)
+button_5 = tk.Button(main, text = "5", padx = 40, pady =20, command = lambda:button_add(5))
+button_5.grid(column = 2,row = 2)
+button_6 = tk.Button(main, text = "6", padx = 40, pady =20, command = lambda:button_add(6))
+button_6.grid(column = 3,row = 2)
+button_7 = tk.Button(main, text = "7", padx = 40, pady =20, command = lambda:button_add(7))
+button_7.grid(column = 1,row = 3)
+button_8 = tk.Button(main, text = "8", padx = 40, pady =20, command = lambda:button_add(8))
+button_8.grid(column = 2,row = 3)
+button_9 = tk.Button(main, text = "9", padx = 40, pady =20, command = lambda:button_add(9))
+button_9.grid(column = 3,row = 3)
+button_0 = tk.Button(main, text = "0", padx = 40, pady =20, command = lambda:button_add(0))
+button_0.grid(column = 2,row = 4)
+button_plus = tk.Button(main, text = "+", padx = 40, pady =20, command = lambda:operator("+"))
+button_plus.grid(column = 1,row = 5)
+button_plus = tk.Button(main, text = "-", padx = 40, pady =20, command = lambda:operator("-"))
+button_plus.grid(column = 2,row = 5)
+button_equal = tk.Button(main, text = "=", padx = 40, pady =20, command = lambda:equal(operation))
+button_equal.grid(column = 3,row = 5)
+
+
+
+main.mainloop()
